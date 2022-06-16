8098bff181c9359daa2739406f2d33e50e1c0c7b
alliano <allianoanonymous@gmail.com>
2022-06-16 17:14:44 +0700

    just for learn

--- src/main/java/com/example/api/controllers/StudentsController.java
+++ src/main/java/com/example/api/controllers/StudentsController.java
@@ -1,6 +1,8 @@
 package com.example.api.controllers;
 
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.api.dto.ResponDataStudent;
+import com.example.api.dto.ResponseSearchStudent;
 import com.example.api.models.entities.Students;
 import com.example.api.services.StudentsService;
 
@@ -30,13 +33,29 @@ public class StudentsController {
    @Autowired
    private StudentsService StudentService;
 
+   /**
+    * @valid adalah anotasi yang di letakan sebagai parameter dari suatu method 
+    * method ini berfungsi untuk memvalidasi apakah data yang dimasukan user/client itu sesuai 
+    * dengan apa yang diinginkan atau tidak .
+    * jikalau data yang dimasukan user/client itu tidak sesuai maka dengan anotasi @valid ini akan 
+    * meng generate error 
+    * @param students
+    * @param errors
+    * @return
+    */
    @PostMapping(path = "")
    public ResponseEntity<ResponDataStudent<Students>> createStudents(@RequestBody @Valid Students students, Errors errors){
       /**
        * kita menginstansiasi ini karna nanti agar kita bisa menggunkan method2 dari class ini
        * dan class ini adalah class yang untuk mengset status gagal atau berhasilnya
        * suatu data di insert ke dalam database
+       * 
+       * ResponseEntity dimaksudkan untuk mewakili seluruh respons HTTP. Anda dapat mengontrol 
+       * apa pun yang masuk ke dalamnya: kode status, header, dan tubuh. @ResponseBody adalah 
+       * penanda untuk badan respons HTTP dan @ResponseStatus menyatakan kode status respons HTTP.
        */
+
+       //ini kelas dari
       ResponDataStudent<Students> responData = new ResponDataStudent<>();
       if (errors.hasErrors()){
         for(ObjectError error : errors.getAllErrors()) {
@@ -62,15 +81,51 @@ public class StudentsController {
       return StudentService.findOne(id); 
    }
    
+   /**
+    * @param student
+    * @param error
+    * @return
+    */
    @PutMapping(path = "")
-   public Students updatStudents(@RequestBody Students student){
-      return StudentService.saveStudents(student);
+   public ResponseEntity<ResponDataStudent<Students>> updatStudents(@RequestBody @Valid Students student, Errors error){
+      
+      ResponDataStudent<Students> resStundet = new ResponDataStudent<>();
+
+      if(error.hasErrors()){
+      for (ObjectError err : error.getAllErrors()) {
+         resStundet.getMessage().add(err.getDefaultMessage());
+      }
+         resStundet.setStatus(false);
+         resStundet.setPayload(null);
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(resStundet);
+      }else{
+         resStundet.setStatus(true);
+         resStundet.setPayload(StudentService.saveStudents(student));
+         return ResponseEntity.status(HttpStatus.OK).body(resStundet);
+      }
    }
 
    @DeleteMapping(path = "{id}")
    public String deleteById(@PathVariable("id") Long id){
       StudentService.deleteStudent(id);
       return "success delete";
+   }
+
+   @PostMapping(path = "{name}")
+   public ResponseEntity<ResponseSearchStudent> search(@PathVariable("name") String name){
+      ResponseSearchStudent status = new ResponseSearchStudent();
+      List<Students> result = StudentService.findByName(name);
+      if (result.size() == 0){
+         status.setMessage(name + " data not found");
+         status.setStatus(false);
+         status.setPayload(null);
+         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(status);
+      }else{
+         status.setMessage("data available");
+         status.setStatus(true);
+         status.setPayload(StudentService.findByName(name));
+         return ResponseEntity.status(HttpStatus.OK).body(status);
+      }
    }
 
 }
---
+++ src/main/java/com/example/api/dto/ResponseSearchStudent.java
@@ -0,0 +1,30 @@
+package com.example.api.dto;
+
+import java.util.List;
+
+import com.example.api.models.entities.Students;
+
+public class ResponseSearchStudent {
+   private String message;
+   private boolean status;
+   private List<Students> payload;
+
+   public void setMessage(String message) {
+      this.message = message;
+   }
+   public String getMessage() {
+      return message;
+   }
+   public void setStatus(boolean status) {
+      this.status = status;
+   }
+   public boolean getStatus(){
+      return this.status;
+   }
+   public void setPayload(List<Students> payload) {
+      this.payload = payload;
+   }
+   public List<Students> getPayload() {
+      return this.payload;
+   }
+}
